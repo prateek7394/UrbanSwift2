62fa871201557a620cec9d046e59482967b2b72f
Prateek Shukla <200108043@hbtu.ac.in>
2024-05-01 17:12:16 +0530

    hello

--- server/server.js
+++ server/server.js
@@ -11,20 +11,20 @@ app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/admin/' , require('./routes/adminRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-const path = require('path')
+// const path = require('path')
 
-if(process.env.NODE_ENV==='production')
-{
+// if(process.env.NODE_ENV==='production')
+// {
 
-    app.use('/' , express.static('client/build'))
+//     app.use('/' , express.static('client/build'))
 
-    app.get('*' , (req , res)=>{
-        
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//     app.get('*' , (req , res)=>{
+//         // console.log(__dirname);
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-    })
+//     })
 
-}
+// }
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
